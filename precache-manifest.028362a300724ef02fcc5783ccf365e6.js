d5e98d82413d4a6680536f51e2665d267045c165
MAKSYM_enko <finside41@gmail.com>
2019-05-24 05:08:03 +0300

    Updates

---
+++ precache-manifest.028362a300724ef02fcc5783ccf365e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549793346581f635dd9aaa0e232edff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ca9f0c8e027ba528d67",
+    "url": "/static/css/main.55f4e0b3.chunk.css"
+  },
+  {
+    "revision": "0695bd80271339efa2c1",
+    "url": "/static/js/2.a5c57b40.chunk.js"
+  },
+  {
+    "revision": "2ca9f0c8e027ba528d67",
+    "url": "/static/js/main.b348c5f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
